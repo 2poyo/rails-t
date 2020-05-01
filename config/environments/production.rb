f90525093d2eaf8e499feaa470ed869e7934eba9
EC2 Default User <ec2-user@ip-172-31-3-29.us-east-2.compute.internal>
2020-05-01 16:05:25 +0000

    Improve production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fierce-thicket-85256.herokuapp.com'
+  host = 'https://fierce-thicket-85256.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
