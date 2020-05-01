0b80bcb148ab95e7693f5f3e80d534f2049bf7a6
EC2 Default User <ec2-user@ip-172-31-3-29.us-east-2.compute.internal>
2020-05-01 13:03:08 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://b2061c2a6d61464e8f18b59ae287d3b4.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
